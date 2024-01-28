625334762c6ce4881fe7c4773375fa4c54d8beb3
Nikhil Reddy <nikhilreddy3111@gmail.com>
2024-01-27 22:32:54 -0500

    ec2 code

--- main.tf
+++ main.tf
@@ -2,10 +2,53 @@ resource "aws_instance" "demo-ec2" {
   ami               = var.ec2_ami
   instance_type     = var.ec2_instance_type
   availability_zone = var.ec2_azs
-  subnet_id         = var.subnet__id
+  subnet_id         = var.subnet1__id
   security_groups   = [var.sg__id]
+  # associate_public_ip_address = true
   tags = {
     "Name" = var.ec2_instance_name
   }
+  user_data = filebase64("${path.module}/ec2-user-data.sh")
+}
 
+resource "aws_launch_template" "demo-launch-template" {
+  name          = var.lt_name
+  image_id      = var.ec2_ami
+  instance_type = var.ec2_instance_type
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    security_groups             = [var.sg__id]
+    associate_public_ip_address = true
+    subnet_id                   = var.subnet1__id
+  }
+
+  # placement {
+  #   availability_zone = var.ec2_azs
+  # }
+
+  # vpc_security_group_ids = [var.sg__id]
+
+  user_data = filebase64("${path.module}/ec2-user-data.sh")
+}
+
+resource "aws_autoscaling_group" "demo-asg" {
+  name = var.asg_name
+  # availability_zones = var.asg_azs
+  desired_capacity    = 1
+  max_size            = 2
+  min_size            = 1
+  vpc_zone_identifier = [var.subnet1__id]
+  launch_template {
+    id      = aws_launch_template.demo-launch-template.id
+    version = aws_launch_template.demo-launch-template.latest_version
+  }
+}
+
+resource "aws_autoscaling_attachment" "demo-asg-attachment" {
+  autoscaling_group_name = aws_autoscaling_group.demo-asg.id
+  lb_target_group_arn    = var.lb__tg__id
 }
--- variables.tf
+++ variables.tf
@@ -15,11 +15,27 @@ variable "ec2_azs" {
   description = "Availability Zones"
   default     = "us-east-1a"
 }
-variable "subnet__id" {
+variable "subnet1__id" {
+  type    = string
+  default = "subnet_id"
+}
+variable "subnet2__id" {
   type    = string
   default = "subnet_id"
 }
 variable "sg__id" {
   type    = string
   default = "sg_id"
+}
+variable "lt_name" {
+  type = string
+}
+variable "asg_azs" {
+  type = list(string)
+}
+variable "asg_name" {
+  type = string
+}
+variable "lb__tg__id" {
+  type = string
 }
